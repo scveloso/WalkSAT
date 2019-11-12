89e9f0e6225087617b863a196264a0d7395eba83
scveloso <samtveloso@gmail.com>
2019-11-11 16:39:33 -0800

    Minor fix and logging

--- walksat.py
+++ walksat.py
@@ -31,15 +31,15 @@ def generate_and_solve_problems():
     while num_clauses < 200:
         set_flips = []
         set_successes = 0
-        print('starting set')
+        print(f'** About to run WalkSAT 50 times with N = {NUM_VARIABLES} and C = {num_clauses} **')
         for i in range(50):
             problem = generate_problem(num_clauses)
             num_flips, success = walksat(problem)
             if (success):
                 set_flips.append(num_flips)
                 set_successes += 1
-        print(statistics.median(set_flips))
-        print(set_successes)
+        print(f'** Finished running WalkSAT 50 times with N = {NUM_VARIABLES} and C = {num_clauses} **')
+        print(f'** Results: Median flips = {statistics.median(set_flips)}, Number of successes = {set_successes}\n\n')
         num_clauses += 20
 
 # Calls makewff to generate a 3-SAT problem with 20 variables and the given
@@ -54,34 +54,43 @@ def generate_problem(num_clauses):
     return parse(problem.decode('utf-8').split("\n")[2:])
 
 # Takes in a 3-SAT problem encoded as an array of strings (e.g. ['20 -19 3 0',
-# '2 -15 -12'] has two clauses) and solves it using WalkSAT
+# '2 -15 -12'] has two clauses) and runs WalkSAT on it for 10 seconds at maximum
+#
+# Returns a pair (number, boolean) of the number of flips and True if the
+# problem was solved, False otherwise
 def walksat(problem):
-    #1. generate random interpretation
-    #2. get unsatisfied clauses and pick a random one
-    #3. pick an atom to flip (randomly 1 or 2)
-        #1. Randomly
-        #2. To minimize # of unsatisfied clauses
-    interpretation = generateInterpretation()
+    # 1. Generate random interpretation
+    interpretation = generate_interpretation()
     end = time() + 10
     num_flips = 0
     while time() < end:
+        # 2. Get unsatisfied clauses
         unsatisfied_clauses = get_unsatisfied_clauses(problem, interpretation)
+
+        # 3. Check if problem is solved
         if(len(unsatisfied_clauses) == 0):
-            #solved problem
             return num_flips, True
+
+        # 4.  Pick one unsatisfied clause at random
         random_unsatisfied_clause = unsatisfied_clauses[randint(0, len(unsatisfied_clauses) - 1)]
-        randomly_flip(interpretation, random_unsatisfied_clause) if(randint(1,2) == 1) else minimizeUnsatisfiedClauses(interpretation, random_unsatisfied_clause, problem)
+
+        # 5. Pick an atom to flip (randomly 5.1 or 5.2)
+            # 5.1 Randomly
+            # 5.2 To minimize # of unsatisfied clauses
+        randomly_flip(interpretation, random_unsatisfied_clause) if(randint(1,2) == 1) else minimize_unsatisfied_clauses(interpretation, random_unsatisfied_clause, problem)
         num_flips += 1
-    #loop timed out
+
+    # Stop with failure after 10 seconds
     return num_flips, False
 
-# Randomly pick a proposition and flip it
+# Randomly pick an atom in the clause and flip it
 def randomly_flip(interpretation, clause):
     randindex = randint(0, 2)
     interpretation[abs(clause[randindex])] *= -1
 
-# Pick an atom to flip to minimize # of unsatisfied clauses
-def minimizeUnsatisfiedClauses(interpretation, the_chosen_clause, problem):
+# Flips one atom in the_chosen_clause that minimizes the # of unsatisfied
+# clauses in the problem
+def minimize_unsatisfied_clauses(interpretation, the_chosen_clause, problem):
     var_to_flip = 0
     min_so_far = 21
 
@@ -92,30 +101,31 @@ def minimizeUnsatisfiedClauses(interpretation, the_chosen_clause, problem):
         if (num_unsatisfied_clauses < min_so_far):
             var_to_flip = var
             min_so_far = num_unsatisfied_clauses
-    
+
     interpretation[abs(var_to_flip)] *= -1
 
-# Generates a random interpretation
-def generateInterpretation():
+# Generates a random interpretation with twenty variables
+def generate_interpretation():
     interpretation = [0]
-    for i in range(1,21):
-        random_ass = i*(-1) if randint(0,1) == 0 else i
+    for i in range(1, 21):
+        random_ass = i * (-1) if randint(0,1) == 0 else i
         interpretation.append(random_ass)
-    
+
     return interpretation
 
 # Takes all clauses and the interpretation so far and returns all unsatisfied
 def get_unsatisfied_clauses(problem, interpretation):
     unsatisfied_clauses = []
     for clause in problem:
-        if(clause[0] != interpretation[abs(clause[0])] and 
-           clause[1] != interpretation[abs(clause[1])] and 
+        if(clause[0] != interpretation[abs(clause[0])] and
+           clause[1] != interpretation[abs(clause[1])] and
            clause[2] != interpretation[abs(clause[2])]):
             unsatisfied_clauses.append(clause)
-    
+
     return unsatisfied_clauses
 
-# Takes generated 3-SAT problem and parses it into a list of list (e.g. ['20 -19 3 0', '2 -15 -12 0'] -> [[20, -19, 3],[2, -15, -12]])
+# Takes generated 3-SAT problem and parses it into a list of list
+# (e.g. ['20 -19 3 0', '2 -15 -12 0'] -> [[20, -19, 3],[2, -15, -12]])
 def parse(problem):
     parsed_problem = []
     for clause in problem[:-1]:
@@ -123,7 +133,7 @@ def parse(problem):
         for var in clause.split(' ')[:-1]:
             parsed_clause.append(int(var))
         parsed_problem.append(parsed_clause)
-    
+
     return parsed_problem
 
 # Calls `make clean` on the terminal to clean up makewff binary executable
